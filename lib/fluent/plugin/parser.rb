caa7c5ee4f73d4adf36561bab6ba5b92f3d466dd
Athish Pranav <athish.pranav@rubrik.com>
2024-07-04 17:12:04 +0530

    parser with UTs over

--- lib/fluent/plugin/parser.rb
+++ lib/fluent/plugin/parser.rb
@@ -12,6 +12,7 @@ module Parser
     end
 
     def get_quota(keys)
+      # Takes a list of keys and returns the quota that matches
       max_score = -1
       quota_to_return = nil
       @quotas.each do |quota|
@@ -27,22 +28,29 @@ module Parser
     private
 
     def parse_quotas
+      # Parses the quotas from the configuration file into Quota objects and stores them in @quotas
       @quotas = @config_file["quotas"].map do |quota|
         Quota.new(quota["name"], quota["description"], quota["group_by"], quota["match_by"], quota["bucket_size"], quota["duration"])
       end
     end
 
     def matching_score(hash1, hash2)
+      # Calculates the matching score between two hashes.
       score = 0
       if hash1.nil? || hash2.nil?
         return 0
       end
       hash1.each do |key, value|
-        score += 1 if hash2[key] == value
+        if hash2[key] == value
+          score += 1
+        else
+          return -1
+        end
       end
       if score == 0
         return -1
       end
       score
     end
+  end
 end
---
+++ test/modules/parser_test.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../../lib/fluent/plugin/parser'
+class ParserTest < Minitest::Test
+  def setup
+
+    @config_data = {
+      "quotas" => [
+        {
+          "name" => "quota1",
+          "description" => "first quota",
+          "group_by" => [ "group1" ],
+          "match_by" => { "group1" => "value1" },
+          "bucket_size" => 100,
+          "duration" => 60
+        },
+        {
+          "name" => "quota2",
+          "description" => "second quota",
+          "group_by" => [ "group1", "group2" ],
+          "match_by" => { "group1" => "value2", "group2" => "value3" },
+          "bucket_size" => 200,
+          "duration" => 120
+        }
+      ]
+    }
+    @config_file_path = @config_data.to_yaml
+    @configuration = Parser::Configuration.new(@config_file_path)
+  end
+
+  def test_parse_quotas
+    refute_nil @configuration.instance_variable_get(:@quotas)
+    assert_equal 2, @configuration.instance_variable_get(:@quotas).length
+  end
+
+  def test_get_quota
+    # Check if the correct quota is retrieved from above definition
+    # UT 1: Subset of groups match fully
+    keys = { "group1" => "value1" , "group2" => "value2" }
+    quota = @configuration.get_quota(keys)
+    assert_equal "quota1", quota.name
+
+    # UT 2: All groups match fully
+    keys = { "group1" => "value2" , "group2" => "value3" }
+    quota = @configuration.get_quota(keys)
+    assert_equal "quota2", quota.name
+
+    # UT 3: Subset of group match partially
+    keys = { "group1" => "value2" , "group2" => "value2" }
+    quota = @configuration.get_quota(keys)
+    assert_nil quota
+
+    # UT 4: None of the group matches
+    keys = { "group1" => "value3" , "group2" => "value2" }
+    quota = @configuration.get_quota(keys)
+    assert_nil quota
+  end
+
+  def test_matching_score
+    # Testing the private helper function
+    score = @configuration.send(:matching_score, { "key1" => "value1" , "key2" => "value2" }, { "key1" => "value1" , "key2" => "value2" })
+    assert_equal 2, score
+
+    score = @configuration.send(:matching_score, { "key1" => "value1" }, { "key2" => "value2" })
+    assert_equal -1, score
+
+    score = @configuration.send(:matching_score, { "key1" => "value1" , "key2" => "value2" }, { "key1" => "value1" , "key2" => "value3" })
+    assert_equal -1, score
+
+    score = @configuration.send(:matching_score, nil, { "key1" => "value1" })
+    assert_equal 0, score
+
+    score = @configuration.send(:matching_score, { "key1" => "value1" }, nil)
+    assert_equal 0, score
+
+    score = @configuration.send(:matching_score, nil, nil)
+    assert_equal 0, score
+  end
+end
