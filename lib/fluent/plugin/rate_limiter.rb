385837263f70569392731bf619ba2ab15fcf20c6
Athish Pranav <athish.pranav@rubrik.com>
2024-07-08 23:14:02 +0530

    Renaming other variables and added comments

--- lib/fluent/plugin/rate_limiter.rb
+++ lib/fluent/plugin/rate_limiter.rb
@@ -1,6 +1,23 @@
+##
+# Rate Limiter module, contains the rate limiting logic
 module RateLimiter
+
+  ##
+  # Bucket class, contains the rate limiting logic for each group
+  # Attributes:
+  #   +bucket_count+: Number of requests in the bucket
+  #   +bucket_last_reset+: Time when the bucket was last reset
+  #   +approx_rate_per_second+: Approximate rate of requests per second
+  #   +rate_last_reset+: Time when the rate was last reset
+  #   +curr_count+: Number of requests in the current second
+  #   +last_warning+: Time when the last warning was issued
+  #   +timeout_s+: Timeout for the bucket
+  #   +bucket_limit+: Maximum number of requests allowed in the bucket
+  #   +bucket_period+: Time period for the bucket
+  #   +rate_limit+: Maximum number of requests allowed per second
   class Bucket
-    attr_accessor :bucket_count, :bucket_last_reset, :approx_rate_per_second, :rate_last_reset, :curr_count, :last_warning, :timeout_s
+    attr_accessor :bucket_count, :bucket_last_reset, :approx_rate_per_second, :rate_last_reset, :curr_count, :last_warning
+    attr_reader :bucket_limit, :bucket_period, :rate_limit, :timeout_s
     def initialize( bucket_limit, bucket_period)
       now = Time.now
       @bucket_count = 0
@@ -15,14 +32,16 @@ module RateLimiter
       @timeout_s = 2*bucket_period
     end
 
+    # Checks if the bucket is free or full
+    # Returns:
+    #   +true+ if the bucket is free
+    #   +false+ if the bucket is full
     def allow
-      # Returns true if the bucket is free
-      # Returns false if the bucket is full
       now = Time.now
       @curr_count += 1
       time_lapsed = now - @rate_last_reset
 
-      if time_lapsed >= 1
+      if time_lapsed.to_i >= 1
         @approx_rate_per_second = @curr_count / time_lapsed
         @rate_last_reset = now
         @curr_count = 0
@@ -43,8 +62,8 @@ module RateLimiter
 
     private
 
+    # Resets the bucket when the window moves to the next time period
     def reset_bucket
-      # Does necessary processing when window moves to next time period
       now = Time.now
       unless @bucket_count == -1 && @approx_rate_per_second > @rate_limit
         @bucket_count = 0
@@ -53,21 +72,31 @@ module RateLimiter
     end
   end
 
+  ##
+  # BucketStore class, organizes the all the group buckets
+  # Attributes:
+  #   +buckets+: Hash containing all the group buckets
   class BucketStore
     def initialize
-      @counters = {}
+      @buckets = {}
     end
 
-    def get_counter(group, quota)
+    # Gets the bucket for the group
+    # Arguments:
+    #   +group+: Group for which the bucket is required
+    #   +quota+: Quota object containing the bucket size and duration
+    def get_bucket(group, quota)
       now = Time.now
-      @counters[group] = @counters.delete(group) || Bucket.new(0, now, 0, 0, now, nil, quota.bucket_size, quota.duration)
+      @buckets[group] = @buckets.delete(group) || Bucket.new( quota.bucket_size, quota.duration)
     end
 
-    def clean_counters
+    # Cleans the buckets that have expired
+    def clean_buckets
       now = Time.now
-      lru_group, lru_counter = @counters.first
-      if !lru_group.nil? && now.to_i - lru_counter.rate_last_reset > lru_counter.timeout_s
-        @counters.delete(lru_group)
+      lru_group, lru_bucket = @buckets.first
+      puts now - lru_bucket.rate_last_reset
+      if !lru_group.nil? && now.to_i - lru_bucket.rate_last_reset.to_i > lru_bucket.timeout_s
+        @buckets.delete(lru_group)
       end
     end
   end
--- test/modules/rate_limiter_test.rb
+++ test/modules/rate_limiter_test.rb
@@ -43,23 +43,24 @@ class TestBucketStore < Minitest::Test
     @quota = ConfigParser::Quota.new("Dummy", "dummy quota for testing",[["group1","a"],["group1","b"]], {["group1","a"] => "value1"}, 10, 2, "reemit")
   end
 
-  def test_get_counter
+  def test_get_bucket
     group = "value1"
-    counter = @bucket_store.get_counter(group, @quota)
-    assert_instance_of RateLimiter::Bucket, counter
-    assert_equal 10, counter.instance_variable_get(:@bucket_limit)
-    assert_equal 2, counter.instance_variable_get(:@bucket_period)
+    bucket = @bucket_store.get_bucket(group, @quota)
+    assert_instance_of RateLimiter::Bucket, bucket
+    assert_equal 10, bucket.instance_variable_get(:@bucket_limit)
+    assert_equal 2, bucket.instance_variable_get(:@bucket_period)
   end
 
-  def test_clean_counters
+  def test_clean_buckets
     group1 = "value1"
-    @bucket_store.get_counter(group1, @quota)
+    @bucket_store.get_bucket(group1, @quota)
     group2 = "value2"
-    @bucket_store.get_counter(group2, @quota)
-    lru_group, lru_counter = @counters.instance_variable_get(:@counters).first
+    @bucket_store.get_bucket(group2, @quota)
+    lru_group, lru_counter = @bucket_store.instance_variable_get(:@buckets).first
     assert_equal group1, lru_group
-    @bucket_store.clean_counters
-    lru_group, lru_counter = @counters.instance_variable_get(:@counters).first
+    sleep(5)
+    @bucket_store.clean_buckets
+    lru_group, lru_counter = @bucket_store.instance_variable_get(:@buckets).first
     assert_equal group2, lru_group
   end
 end
