e638292408b146a3ad9f9e48c3a4835842332447
Athish Pranav <athish.pranav@rubrik.com>
2024-07-09 21:20:53 +0530

    wrote basic filter

--- lib/fluent/plugin/filter_quota_throttle.rb
+++ lib/fluent/plugin/filter_quota_throttle.rb
@@ -1,5 +1,7 @@
 require 'fluent/plugin/filter'
 require_relative './parser'
+require_relative './matcher'
+require_relative './rate_limiter'
 module Fluent::Plugin
   class QuotaThrottleFilter < Filter
     Fluent::Plugin.register_filter('quota_throttle', self)
@@ -16,19 +18,39 @@ module Fluent::Plugin
       @config = Parser::Configuration.new(@path)
       raise "quota config file should not be empty" \
         if @path.nil?
+      @match_helper = Matcher::Match_Helper.new(@config.quotas, @config.default_quota)
     end
 
     def start
       super
+      @bucket_store = RateLimiter::BucketStore.new
     end
 
     def shutdown
       super
+      log.info "Shutting down"
     end
 
     def filter(tag, time, record)
-      record
+      @bucket_store.clean_buckets
+      quota = @match_helper.get_quota(record)
+      group = quota.group_by.map { |key| record.dig(*key) }
+      bucket = @bucket_store.get_bucket(group, quota)
+      if bucket.allow
+        record
+      else
+        quota_breached(bucket)
+        nil
+      end
     end
+
+    private
+
+    def quota_breached(bucket, quota)
+      if bucket.last_warning.nil? || Time.now - bucket.last_warning > 60
+        log.warn "Quota breached"
+        bucket.last_warning = Time.now
+      end
 
   end
 end
