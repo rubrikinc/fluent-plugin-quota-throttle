ac53f442376d6c9bed8a7478aca4b0425be9b619
Athish Pranav <athish.pranav@rubrik.com>
2024-07-11 12:00:01 +0530

    basic metrics todo inclusions done

--- fluent-plugin-quota-throttle.gemspec
+++ fluent-plugin-quota-throttle.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "mutex_m"
 
   spec.add_runtime_dependency "fluentd"
+  spec.add_runtime_dependency "fluent-plugin-prometheus"
 end
--- lib/fluent/plugin/filter_quota_throttle.rb
+++ lib/fluent/plugin/filter_quota_throttle.rb
@@ -1,4 +1,5 @@
 require 'fluent/plugin/filter'
+require 'fluent/plugin/prometheus'
 require_relative 'config_parser'
 require_relative 'matcher'
 require_relative 'rate_limiter'
@@ -11,6 +12,9 @@ module Fluent::Plugin
   # QuotaThrottleFilter class is derived from the Filter class and is responsible for filtering records based on quotas
   class QuotaThrottleFilter < Filter
     Fluent::Plugin.register_filter('quota_throttle', self)
+    include Fluent::Plugin::PrometheusLabelParser
+    include Fluent::Plugin::Prometheus
+    attr_reader :registry
 
     desc "Path for the quota config file"
     config_param :path, :string, :default => nil
@@ -18,9 +22,14 @@ module Fluent::Plugin
     desc "Delay in seconds between warnings for the same group when the quota is breached"
     config_param :warning_delay, :time, :default => 60
 
+    desc "Enable prometheus metrics"
+    config_param :enable_metrics, :bool, :default => false
+
     def initialize
-      @reemit_tag_suffix = "secondary"
       super
+      @reemit_tag_suffix = "secondary"
+
+      @registry = Prometheus::Client.registry if @enable_metrics
     end
 
     # Configures the plugin
@@ -32,11 +41,25 @@ module Fluent::Plugin
         if @warning_delay < 0
       @config = ConfigParser::Configuration.new(@path)
       @match_helper = Matcher::MatchHelper.new(@config.quotas, @config.default_quota)
+      if @enable_metrics
+        expander_builder = Fluent::Plugin::Prometheus.placeholder_expander(log)
+        expander = expander_builder.build({})
+        @base_labels = parse_labels_elements(conf)
+        @base_labels.each do |key, value|
+          unless value.is_a?(String)
+            raise Fluent::ConfigError, "record accessor syntax is not available in metric labels for quota throttle plugin"
+          end
+          @base_labels[key] = expander.expand(value)
+        end
+      end
     end
 
     def start
       super
       @bucket_store = RateLimiter::BucketStore.new
+      if @enable_metrics
+        @metrics = {quota_usage: get_counter(:fluentd_quota_throttle_usage, "Quota usage for each group when throttling is applied")}
+      end
     end
 
     def shutdown
@@ -58,7 +81,7 @@ module Fluent::Plugin
       if bucket.allow
         record
       else
-        quota_breached(tag, time, record, bucket, quota)
+        quota_breached(bucket, quota, time)
         nil
       end
     end
@@ -70,7 +93,7 @@ module Fluent::Plugin
     #   +bucket+: (Bucket) The bucket that has breached the quota
     #   +quota+: (Quota) The quota that has been breached
     #   +timestamp+: (Time) The timestamp of the record
-    def quota_breached(tag, timestamp, record, bucket, quota)
+    def quota_breached(bucket, quota, timestamp)
       if bucket.last_warning.nil? || Time.now - bucket.last_warning > @warning_delay
         log.warn "Quota breached for group #{bucket.group} in quota #{quota.name}"
         bucket.last_warning = Time.now
@@ -80,8 +103,16 @@ module Fluent::Plugin
         log.debug "Dropping record"
       when "reemit"
         log.debug "Reemitting record"
-        new_tag = "#{@reemit_tag_preffix}.#{tag}"
+        new_tag = "#{tag}.#{@reemit_tag_suffix}"
         router.emit(new_tag, timestamp, record)
+      end
+    end
+
+    def get_counter(name, docstring)
+      if @registry.exist?(name)
+        @registry.get(name)
+      else
+        @registry.counter(name, docstring: docstring, base_labels: @base_labels.keys)
       end
     end
   end
