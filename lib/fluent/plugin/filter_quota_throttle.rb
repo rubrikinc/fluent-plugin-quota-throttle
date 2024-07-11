01e5e7d44019312e1947a7f35ccc6ec1d5672de6
Athish Pranav <athish.pranav@rubrik.com>
2024-07-11 14:54:40 +0530

    changed retagging from suffix to prefix to ease the handle of new tags

--- lib/fluent/plugin/filter_quota_throttle.rb
+++ lib/fluent/plugin/filter_quota_throttle.rb
@@ -11,7 +11,6 @@ module Fluent::Plugin
   # QuotaThrottleFilter class is derived from the Filter class and is responsible for filtering records based on quotas
   class QuotaThrottleFilter < Filter
     Fluent::Plugin.register_filter('quota_throttle', self)
-    helpers :event_emitter
 
     desc "Path for the quota config file"
     config_param :path, :string, :default => nil
@@ -81,7 +80,7 @@ module Fluent::Plugin
         log.debug "Dropping record"
       when "reemit"
         log.debug "Reemitting record"
-        new_tag = "#{tag}.#{@reemit_tag_suffix}"
+        new_tag = "#{@reemit_tag_preffix}.#{tag}"
         router.emit(new_tag, timestamp, record)
       end
     end
